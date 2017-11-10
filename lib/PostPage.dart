4452c42e05236a8d7fccb6b6bbd63b56b51b1127
Tomasz Polański <polanski.tomek@gmail.com>
2017-11-10 19:25:09 +0100

    Formatting

--- lib/PostPage.dart
+++ lib/PostPage.dart
@@ -31,46 +31,48 @@ class PostPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-        key: _scaffoldKey,
-        backgroundColor: Colors.white,
-        body: new CustomScrollView(
-          slivers: <Widget>[
-            new SliverAppBar(
-                floating: true,
-                title: buildTitle(post.title, Theme
-                    .of(context)
-                    .textTheme
-                    .title)
-            ),
-            new SliverList(
-              delegate: new SliverChildListDelegate(<Widget>[
-                new PhotoHero(
-                  photo: post.imageUrl,
-                  onTap: () {
-                    Navigator.of(context).pop();
-                  },
-                ),
-                new Container(
-                  padding: const EdgeInsets.symmetric(vertical: 16.0, horizontal: 10.0),
-                  child:
-                  new RichText(
-                    textAlign: TextAlign.justify,
-                    text: new TextSpan(
-                        children: post.text.expand((it) =>
-                            it.spans.map((span) =>
-                            new TextSpan(
-                                text: span.text,
-                                style: _getStyle(span.type))
-                            ))
-                            .toList()
-                    ),
+      key: _scaffoldKey,
+      backgroundColor: Colors.white,
+      body: new CustomScrollView(
+        slivers: <Widget>[
+          new SliverAppBar(
+              floating: true,
+              title: buildTitle(post.title, Theme
+                  .of(context)
+                  .textTheme
+                  .title)
+          ),
+          new SliverList(
+            delegate: new SliverChildListDelegate(<Widget>[
+              new PhotoHero(
+                photo: post.imageUrl,
+                onTap: () {
+                  Navigator.of(context).pop();
+                },
+              ),
+              new Container(
+                padding: const EdgeInsets.symmetric(
+                    vertical: 16.0, horizontal: 10.0),
+                child:
+                new RichText(
+                  textAlign: TextAlign.justify,
+                  text: new TextSpan(
+                      children: post.text.expand((it) =>
+                          it.spans.map((span) =>
+                          new TextSpan(
+                              text: span.text,
+                              style: _getStyle(span.type))
+                          ))
+                          .toList()
                   ),
                 ),
-              ],
               ),
+            ],
             ),
-          ],
-        ));
+          ),
+        ],
+      ),
+    );
   }
 }
 
