aba34a43575814e9fb9cc0cfb2df89f6289fef82
Tomasz Polański <polanski.tomek@gmail.com>
2017-11-10 20:01:15 +0100

    Improved transition performance

--- lib/PostPage.dart
+++ lib/PostPage.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:ulicastefankowa/PhotoHero.dart';
 import 'package:ulicastefankowa/utlis/TextUtils.dart';
@@ -8,13 +10,35 @@ const double _kFlexibleSpaceMaxHeight = 200.0;
 const TextStyle _kBodyFont = const TextStyle(
     fontFamily: "Serif", fontSize: 20.0, wordSpacing: 4.0, color: Colors.black);
 
-class PostPage extends StatelessWidget {
+class PostPage extends StatefulWidget {
   const PostPage({this.post, Key key}) : super(key: key);
 
   final Post post;
 
+  @override
+  _PostPageState createState() => new _PostPageState();
+
+}
+
+class _PostPageState extends State<PostPage> {
+
   static final GlobalKey<ScaffoldState> _scaffoldKey =
   new GlobalKey<ScaffoldState>();
+  bool _showText = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _delayText();
+  }
+
+  Future _delayText() async {
+    // TODO Temporary solution until connected to navigation transition callback
+    await new Future.delayed(const Duration(milliseconds: 500));
+    setState(() {
+      _showText = true;
+    });
+  }
 
   TextStyle _getStyle(String type) {
     switch (type) {
@@ -37,7 +61,7 @@ class PostPage extends StatelessWidget {
         slivers: <Widget>[
           new SliverAppBar(
               floating: true,
-              title: buildTitle(post.title, Theme
+              title: buildTitle(widget.post.title, Theme
                   .of(context)
                   .textTheme
                   .title)
@@ -45,19 +69,19 @@ class PostPage extends StatelessWidget {
           new SliverList(
             delegate: new SliverChildListDelegate(<Widget>[
               new PhotoHero(
-                photo: post.imageUrl,
+                photo: widget.post.imageUrl,
                 onTap: () {
                   Navigator.of(context).pop();
                 },
               ),
-              new Container(
+              _showText ? new Container(
                 padding: const EdgeInsets.symmetric(
                     vertical: 16.0, horizontal: 10.0),
                 child:
                 new RichText(
                   textAlign: TextAlign.justify,
                   text: new TextSpan(
-                      children: post.text.expand((it) =>
+                      children: widget.post.text.expand((it) =>
                           it.spans.map((span) =>
                           new TextSpan(
                               text: span.text,
@@ -66,7 +90,8 @@ class PostPage extends StatelessWidget {
                           .toList()
                   ),
                 ),
-              ),
+              )
+                  : new Container(),
             ],
             ),
           ),
