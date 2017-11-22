07b5aef1750ccd67c174ceed211f6a9a90523239
Tomek Polański <tomasz.polanski@futurice.com>
2017-11-22 22:13:17 +0100

    Improving about (#11)

--- lib/i18n/Localizations.dart
+++ lib/i18n/Localizations.dart
@@ -25,6 +25,16 @@ class CustomLocalizations {
       'appSourceCode': '.\n\nTo see the source code for this app, please visit the ',
       'appRepoLink': 'Ulica Stefankowa Github repo',
       'postFontSize': "Post's text size",
+      'aboutText': """TODO: Moi Drodzy Czytelnicy,
+
+pomysł bloga, na którym umieszczać będę kolejne przygody prosiaczka Stefana, a z czasem również figielki i psoty innych bajkowych bohaterów, powstał z mojej pasji do opisywania świata tak,  jak ja go widzę, a może tak, jak widzieć bym go chciała…. 
+
+Jako że Wy – młodzi czytelnicy – jesteście szczególnie wymagający, to dla Was chciałabym tworzyć i Wasze opinie – mam nadzieję – poznać.
+
+Zapraszam Was serdecznie do lektury zamieszczonych bajeczek, pięknie ilustrowanych przez Paulę Dudek (www.behance.net/paula_dudek).
+
+By nie przegapić żadnych szczegółów z życia prosiaczka, proponuję czytać bajeczki po kolei, od najwcześniejszej""",
+      'aboutBlog': 'About',
     },
     'pl': {
       'title': 'UliCa SteFAnkOwA',
@@ -36,18 +46,40 @@ class CustomLocalizations {
       'appSourceCode': '.\n\nŻeby zobaczyć źródła tej aplikacji, prosze odwiedzić stronę ',
       'appRepoLink': "Ulicy Stefankowej na Github'ie",
       'postFontSize': "Wielkość czcionki artykułu",
+      'aboutText': """Moi Drodzy Czytelnicy,
+
+pomysł bloga, na którym umieszczać będę kolejne przygody prosiaczka Stefana, a z czasem również figielki i psoty innych bajkowych bohaterów, powstał z mojej pasji do opisywania świata tak,  jak ja go widzę, a może tak, jak widzieć bym go chciała…. 
+
+Jako że Wy – młodzi czytelnicy – jesteście szczególnie wymagający, to dla Was chciałabym tworzyć i Wasze opinie – mam nadzieję – poznać.
+
+Zapraszam Was serdecznie do lektury zamieszczonych bajeczek, pięknie ilustrowanych przez Paulę Dudek (www.behance.net/paula_dudek).
+
+By nie przegapić żadnych szczegółów z życia prosiaczka, proponuję czytać bajeczki po kolei, od najwcześniejszej""",
+      'aboutBlog': 'O Blogu',
     },
   };
 
   String get title => _values[locale.languageCode]['title'];
+
   String get useLightTheme => _values[locale.languageCode]['useLightTheme'];
+
   String get slowAnimations => _values[locale.languageCode]['slowAnimations'];
+
   String get appVersion => _values[locale.languageCode]['appVersion'];
+
   String get appCopy => _values[locale.languageCode]['appCopy'];
+
   String get appDescription => _values[locale.languageCode]['appDescription'];
+
   String get appSourceCode => _values[locale.languageCode]['appSourceCode'];
+
   String get appRepoLink => _values[locale.languageCode]['appRepoLink'];
+
   String get postFontSize => _values[locale.languageCode]['postFontSize'];
+
+  String get aboutText => _values[locale.languageCode]['aboutText'];
+
+  String get aboutBlog => _values[locale.languageCode]['aboutBlog'];
 
 }
 
