3ddf26979aa8019815cb3c7d25d8307119b24387
Tomek Polanski <tpolanski@groupon.com>
2020-12-15 13:14:41 +0100

    Null migration

---
+++ lib/app.dart
@@ -0,0 +1,97 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:rxdart/rxdart.dart';
+import 'package:rxdart/streams.dart';
+import 'package:rxdart/subjects.dart';
+import 'package:rxdart/transformers.dart';
+import 'package:ulicastefankowa/features/home/home_page.dart';
+import 'package:ulicastefankowa/injection/injector.dart';
+import 'package:ulicastefankowa/shared/storage/settings.dart';
+
+final ThemeData _kGalleryLightTheme = ThemeData(
+  brightness: Brightness.light,
+  primarySwatch: Colors.orange,
+  primaryColor: Colors.white,
+);
+
+final ThemeData _kGalleryDarkTheme = ThemeData(
+  brightness: Brightness.dark,
+  primarySwatch: Colors.orange,
+);
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key key}) : super(key: key);
+
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  Settings _settings;
+
+  final SettingsProvider settings = SettingsProvider();
+  final PublishSubject<Settings> _saveSettingsSubject = PublishSubject();
+  StreamSubscription<Settings> _saveSettingsSubscription;
+
+  @override
+  void initState() {
+    _settings = settings.initial;
+    readSettings();
+    _saveSettingsSubscription = _saveSettingsSubject.stream
+        .debounce((_) => TimerStream(true, const Duration(seconds: 1)))
+        .listen(settings.saveSettings);
+    super.initState();
+  }
+
+  Future<void> readSettings() async {
+    final s = await settings.readSettings();
+    setState(() {
+      _settings = s;
+    });
+  }
+
+  @override
+  void dispose() {
+    _saveSettingsSubscription.cancel();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      onGenerateTitle: (c) => AppLocalizations.of(c).title,
+      localizationsDelegates: const [
+        AppLocalizations.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: const [
+        Locale('en', ''),
+        Locale('pl', ''),
+      ],
+      theme: _settings.useLightTheme ? _kGalleryLightTheme : _kGalleryDarkTheme,
+      home: HomePage(
+        prismic: Injection.of(context).prismic,
+        useLightTheme: _settings.useLightTheme,
+        onThemeChanged: (bool value) {
+          setState(() {
+            _settings = _settings.copyWith(useLightTheme: value);
+          });
+          _saveSettingsSubject.add(_settings);
+        },
+        fontSize: _settings.textSize,
+        onFontSizeChanged: (double value) {
+          setState(() {
+            _settings = _settings.copyWith(textSize: value);
+          });
+
+          _saveSettingsSubject.add(_settings);
+        },
+      ),
+    );
+  }
+}
