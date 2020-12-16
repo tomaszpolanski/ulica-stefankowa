471840dc89637244d90a70e8a66315bc2dbe0e49
Tomek Polanski <tpolanski@groupon.com>
2020-12-16 17:34:29 +0100

    Update main_mobile.dart

--- lib/main_mobile.dart
+++ lib/main_mobile.dart
@@ -1,6 +1,8 @@
 // ignore: import_of_legacy_library_into_null_safe
 import 'package:firebase_analytics/firebase_analytics.dart';
+// ignore: import_of_legacy_library_into_null_safe
 import 'package:firebase_analytics/observer.dart';
+// ignore: import_of_legacy_library_into_null_safe
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:ulicastefankowa/app.dart';
