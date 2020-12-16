f1e06ee59717064cfdf73ceb03e16fc6e72866aa
Tomek Polanski <tpolanski@groupon.com>
2020-12-16 17:20:14 +0100

    wip

--- lib/injection/injector.dart
+++ lib/injection/injector.dart
@@ -1,3 +1,7 @@
+// ignore: import_of_legacy_library_into_null_safe
+import 'package:firebase_analytics/firebase_analytics.dart';
+// ignore: import_of_legacy_library_into_null_safe
+import 'package:firebase_analytics/observer.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:ulicastefankowa/shared/network/prismic.dart';
 import 'package:ulicastefankowa/shared/security/environment.dart';
@@ -6,17 +10,25 @@ abstract class Injector {
   Prismic get prismic;
 
   Env get environment;
+
+  List<RouteObserver<PageRoute<dynamic>>> get routeObservers;
 }
 
 class InjectorImpl implements Injector {
-  InjectorImpl({required this.environment})
-      :  prismic = PrismicImpl(environment);
+  InjectorImpl({
+    required this.environment,
+    required FirebaseAnalytics analytics,
+  })   : prismic = PrismicImpl(environment),
+        routeObservers = [FirebaseAnalyticsObserver(analytics: analytics)];
 
   @override
   final Env environment;
 
   @override
   final Prismic prismic;
+
+  @override
+  final List<RouteObserver<PageRoute<dynamic>>> routeObservers;
 }
 
 class Injection extends InheritedWidget implements Injector {
@@ -37,6 +49,10 @@ class Injection extends InheritedWidget implements Injector {
 
   @override
   Prismic get prismic => _injector.prismic;
+
+  @override
+  List<RouteObserver<PageRoute<dynamic>>> get routeObservers =>
+      _injector.routeObservers;
 
   @override
   bool updateShouldNotify(covariant InheritedWidget oldWidget) => false;
--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,18 @@
+// ignore: import_of_legacy_library_into_null_safe
+import 'package:firebase_analytics/firebase_analytics.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:ulicastefankowa/app.dart';
 import 'package:ulicastefankowa/injection/injector.dart';
 import 'package:ulicastefankowa/shared/security/environment.dart';
 
-void main() {
-  final injector = InjectorImpl(environment: EnvImpl());
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  final injector = InjectorImpl(
+    environment: EnvImpl(),
+    analytics: FirebaseAnalytics(),
+  );
   runApp(
     Injection(
       injector,
